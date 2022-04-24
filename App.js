4c8ec2755aafc537b585eff73cceca6769f0655d
21stChrom <parkerjmartin2020@gmail.com>
2022-04-24 01:32:24 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </head>
+        
+      </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-gplgtw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-gplgtw)
